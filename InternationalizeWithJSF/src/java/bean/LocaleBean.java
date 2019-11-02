3929d388e199b6e89046882adc62cc73c5debfd0
khalid7487 <khalid15-7487@diu.edu.bd>
2019-11-02 19:48:11 +0600

    locale bean

---
+++ InternationalizeWithJSF/src/java/bean/LocaleBean.java
@@ -0,0 +1,37 @@
+
+package bean;
+import java.util.*;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+public class LocaleBean {
+    private final Locale ENGLISH=Locale.ENGLISH;
+    private final Locale SPANISH=new Locale("es");
+    private final Locale FRANCH=Locale.FRANCE;
+    private final Locale BANGLA=new Locale("bn");
+    private Locale locale=ENGLISH;
+    public Locale getLocale(){
+        return (locale);
+    }
+    public void setBangla(ActionEvent event){
+        locale=BANGLA;
+        updateViewLoclae();
+    }
+    public void setEnglish(ActionEvent event){
+        locale=ENGLISH;
+        updateViewLoclae();
+    }
+    public void setSpanish(ActionEvent event){
+        locale=SPANISH;
+        updateViewLoclae();
+    }
+    public void setFrench(ActionEvent event){
+        locale=FRANCH;
+        updateViewLoclae();
+    }
+    
+    private void updateViewLoclae(){
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+    }
+     
+    
+}
